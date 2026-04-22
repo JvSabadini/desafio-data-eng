61182d0b59defd2e7f47a37eec3e3563c2ea780c
JvSabadini <47692553+JvSabadini@users.noreply.github.com>
2026-04-22 11:04:24 -0300

    Update verificar_dados.py

--- src/verificar_dados.py
+++ src/verificar_dados.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 DATABASE_URI = 'postgresql+psycopg2://retize_user:retize_password@localhost:5432/retize_db'
 engine = create_engine(DATABASE_URI)
 
-# Podes mudar a query para o que quiseres testar
 query = "SELECT * FROM refined.fct_posts LIMIT 10;"
 
 df = pd.read_sql(query, engine)
